0c2f684f3c6d17a32ffe86a22389b34fef010959
sebastian <barrerarodriguezsebastian@gmail.com>
2021-09-14 13:53:39 -0500

    Funcion saludar

---
+++ pruebaGit/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pruebaGit
+{
+    class Class1
+    {
+        public static string saludar(string nombre) 
+        {
+            string ola = "Hola " + nombre;
+            return ola;
+        
+        }
+    }
+}
--- pruebaGit/Form1.cs
+++ pruebaGit/Form1.cs
@@ -16,5 +16,10 @@ namespace pruebaGit
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = Class1.saludar(textBox1.Text);
+        }
     }
 }
